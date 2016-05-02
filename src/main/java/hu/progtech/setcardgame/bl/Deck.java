8c8668cbb498b995118a74050f49a75005ea9e22
szabom5 <szabo.marica@gmail.com>
2016-05-02 22:07:04 +0200

    JavaDoc Deck

--- src/main/java/hu/progtech/setcardgame/bl/Deck.java
+++ src/main/java/hu/progtech/setcardgame/bl/Deck.java
@@ -1,19 +1,35 @@
 package hu.progtech.setcardgame.bl;
 
+/**
+ * Created by marianna on 2016.04.09..
+ */
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
-
 /**
- * Created by marianna on 2016.04.09..
+ * Deck class represents the deck of Cards.
  */
+
 public class Deck {
+
+    /**
+     * The List of Cards to maintain the deck.
+     */
 
     private List<Card> deck = new ArrayList<>();
 
+    /**
+     * The index of the next Card to be placed on the board.
+     */
+
     private int indexOfNextCard;
+
+    /**
+     * Initialises a newly created Deck object.
+     */
 
     public Deck() {
         makeNewDeck();
@@ -21,10 +37,20 @@ public class Deck {
         indexOfNextCard = 0;
     }
 
+    /**
+     * Constructs a new Deck object by setting the list of Cards and resetting the index.
+     * @param list The List of Cards to maintain the deck
+     */
+
     public Deck(List<Card> list) {
         deck = list;
         indexOfNextCard = 0;
     }
+
+    /**
+     * Returns the next Card to be placed onto the Board.
+     * @return The next Card to be placed
+     */
 
     public Card getNextCard() {
         Card card = null;
@@ -36,9 +62,18 @@ public class Deck {
         return card;
     }
 
+    /**
+     * Resets the index of the next Card to be placed.
+     */
+
     public void resetNextCard() {
         indexOfNextCard = 0;
     }
+
+    /**
+     * Fills the deck with all the 81 Cards that can be found in the Set Card Game.
+     */
+
     private void makeNewDeck() {
 
         for(int number = 0; number < 3; number++) {
@@ -53,9 +88,18 @@ public class Deck {
 
     }
 
+    /**
+     * Shuffeles the deck, rearranging the Cards in a random order.
+     */
+
     private void suffleAllCards() {
         Collections.shuffle(deck,new Random());
     }
+
+    /**
+     * Returns the list of Cards represented.
+     * @return The list of Cards
+     */
 
     public List<Card> getDeck() {
         return deck;
