9c642dcbfdae0996b790615b5488759eb0fe74bb
szabom5 <szabo.marica@gmail.com>
2016-05-09 13:35:34 +0200

    Javadoc refactor XMLHandler

--- src/main/java/hu/progtech/setcardgame/dao/XMLHandler.java
+++ src/main/java/hu/progtech/setcardgame/dao/XMLHandler.java
@@ -18,36 +18,36 @@ public interface XMLHandler {
     /**
      * Writes into the leaderBoard xml file the score of the latest game.
      *
-     * @param score the Score of the latest game.
+     * @param score the {@link hu.progtech.setcardgame.bl.Score} of the latest game.
      */
 
-    public void writeScore(Score score);
+    void writeScore(Score score);
 
     /**
-     * Returns a list of Score elements, which will build the leader board up.
-     * @return a list of Score elements.
+     * Returns a list of {@link hu.progtech.setcardgame.bl.Score} elements, which will build the leader board up.
+     * @return a list of {@link hu.progtech.setcardgame.bl.Score} elements.
      */
 
-    public List<Score> readHighScoreTable();
+    List<Score> readHighScoreTable();
 
     /**
-     * Returns the requested pre-shuffled deck.
-     * @param n the n-th deck in the list of pre-shuffled decks.
-     * @return the requested pre-shuffled deck.
+     * Returns the requested pre-shuffled {@link hu.progtech.setcardgame.bl.Deck}.
+     * @param n the n-th deck in the list of pre-shuffled {@link hu.progtech.setcardgame.bl.Deck}s.
+     * @return the requested pre-shuffled {@link hu.progtech.setcardgame.bl.Deck}.
      */
 
-    public Deck readNextDeck(int n);
+    Deck readNextDeck(int n);
 
     /**
      * Returns the position of the player in the sorted leaderBoard xml.
-     * @param player the subject of the search
+     * @param player the subject of the search.
      * @return the position of the player element, if not found returns -1.
      */
-    public int positionOfPlayerInXML(String player);
+    int positionOfPlayerInXML(String player);
 
     /**
-     * Writes n pre-shuffeled Decks into the listOfDecks.xml file.
-     * @param list the list of decks to be written into the xml.
+     * Writes n pre-shuffeled {@link hu.progtech.setcardgame.bl.Deck}s into the listOfDecks.xml file.
+     * @param list the list of {@link hu.progtech.setcardgame.bl.Deck}s to be written into the xml.
      */
-    public void writePreShuffeledDecks(List<Deck> list);
+    void writePreShuffeledDecks(List<Deck> list);
 }
